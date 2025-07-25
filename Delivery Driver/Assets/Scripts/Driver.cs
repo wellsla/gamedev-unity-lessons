c866c808758b5f1a6c0d49961d625f31cdd7e719
wellsla <61843654+wellsla@users.noreply.github.com>
2025-07-25 00:18:53 -0300

    :sparkles: feat: Basic scripts and features tested

---
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 240f;
+    [SerializeField] float moveSpeed = 10f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
