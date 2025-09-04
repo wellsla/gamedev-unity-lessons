2a036a78d4deda909a0803a8c165ef9af6bc01ac
wellsla <61843654+wellsla@users.noreply.github.com>
2025-09-04 15:24:56 -0300

    3D game dev lesson number 28 - trigger projectiles

---
+++ Obstacle Dodge/Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject projectile1;
+    [SerializeField] GameObject projectile2;
+    [SerializeField] GameObject projectile3;
+    [SerializeField] GameObject projectile4;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            projectile.SetActive(true);
+            projectile1.SetActive(true);
+            projectile2.SetActive(true);
+            projectile3.SetActive(true);
+            projectile4.SetActive(true);
+        }
+    }
+}
