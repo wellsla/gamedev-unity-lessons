b0210891f696fbcadcd632ca042a90ccd34880c4
wellsla <61843654+wellsla@users.noreply.github.com>
2025-09-04 10:30:10 -0300

    3D game dev lesson number 20 - get component

--- Obstacle Dodge/Assets/Scripts/ObjectHit.cs
+++ Obstacle Dodge/Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
+        GetComponent<MeshRenderer>().material.color = Color.black;
         Debug.Log("Something hit me!");
     }
 }
